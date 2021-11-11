1ba6f3b39260de17abcc1bf6ad110bdf1aebd861
Michese <michese48@mail.ru>
2021-11-11 10:50:15 +0300

    initial project

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,12 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
   },
+  plugins: [
+    "prettier",
+    "vue",
+  ],
   rules: {
+    "prettier/prettier": "error",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
