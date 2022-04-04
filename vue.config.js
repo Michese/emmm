fda9bc6304cb58e8d58aa1b9ecfc2a99fefbc535
Michese <michese48@mail.ru>
2022-04-04 08:56:04 +0300

    minimized product version

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,41 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const HtmlWebpackPlugin = require('vue-html-webpack-plugin');
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
 module.exports = {
   publicPath: '.',
   lintOnSave: process.env.NODE_ENV !== 'production',
   productionSourceMap: process.env.NODE_ENV !== 'production',
+  css: { extract: false },
+  configureWebpack: {
+    optimization: {
+      splitChunks: false,
+      minimize: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        vue: true,
+        inlineSource: '.(js|css)$',
+        inject: "body",
+        minify: {
+          html5                          : true,
+          collapseWhitespace             : true,
+          minifyCSS                      : true,
+          minifyJS                       : true,
+          minifyURLs                     : false,
+          removeAttributeQuotes          : true,
+          removeComments                 : true, // false for Vue SSR to find app placeholder
+          removeEmptyAttributes          : true,
+          removeOptionalTags             : true,
+          removeRedundantAttributes      : true,
+          removeScriptTypeAttributes     : true,
+          removeStyleLinkTypeAttributese : true,
+          useShortDoctype                : true
+        },
+      }),
+      new HtmlWebpackInlineSourcePlugin(),
+    ],
+  },
 };
